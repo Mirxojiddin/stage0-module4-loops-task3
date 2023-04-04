53675f3f20120e200216b9eafdf06e975a754347
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-04-04 21:35:50 +0500

    javob 2

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -3,6 +3,6 @@ package school.mjc.stage0.loops.task3;
 public class AlphabetPrinter {
     public void printAlphabet() {
         for (char i='a'; i<='z'; i++)
-            System.out.print(i);
+            System.out.println(i);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -3,6 +3,7 @@ package school.mjc.stage0.loops.task3;
 public class DigitsSum {
     public void printDigitsSum(int t){
         int len =0, sumDigit=0;
+        t=t<0?t*-1:t;
         int tt=t;
         for (int i=0; i<tt; i++){
             len++;
@@ -10,8 +11,6 @@ public class DigitsSum {
             if (t==0)
                 break;
         }
-        System.out.println(len);
-
         for (int i=0; i<len; i++){
             sumDigit += tt%10;
             tt/=10;
